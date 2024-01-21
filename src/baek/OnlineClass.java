42f7934e06d672c503526596b3cefbbda05d7d0f
Griotold <gotjd9773@naver.com>
2024-01-21 12:26:34 +0900

    백 - 4. Optional - Optional은 리턴타입에만 사용하자

--- src/baek/OnlineClass.java
+++ src/baek/OnlineClass.java
@@ -1,12 +1,15 @@
 package baek;
 
+import java.util.Optional;
+
 public class OnlineClass {
 
     private Integer id;
     private String title;
     private boolean closed;
-
     private Progress progress;
+
+
 
     public OnlineClass(Integer id, String title, boolean closed) {
         this.id = id;
@@ -38,8 +41,8 @@ public class OnlineClass {
         this.closed = closed;
     }
 
-    public Progress getProgress() {
-        return progress;
+    public Optional<Progress> getProgress() {
+        return Optional.ofNullable(progress);
     }
 
     public void setProgress(Progress progress) {
--- src/baek/optional/OptionalMain.java
+++ src/baek/optional/OptionalMain.java
@@ -5,6 +5,7 @@ import baek.Progress;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class OptionalMain {
 
@@ -17,12 +18,16 @@ public class OptionalMain {
         springClasses.add(new OnlineClass(5, "rest api development", true));
 
         // 새롭게 추가한 progress는 값이 있을 수도 있고 null일 수도
-        // 1. 기존의 해결방법 -> 에러를 만들기 좋은 코드!
         OnlineClass msa = new OnlineClass(6, "MSA", true);
-        Progress progress = msa.getProgress();
-        if (progress != null) {
-            System.out.println(progress.getStudyDuration());
-        }
+        // 1. 기존의 해결방법 -> 에러를 만들기 좋은 코드!
+//        Progress progress = msa.getProgress();
+//        if (progress != null) {
+//            System.out.println(progress.getStudyDuration());
+//        }
+
+        // 2. 자바 8 이후 해결방법 - Optional -> 리턴 타입에만 쓸 것을 권장
+        Optional<Progress> progress = msa.getProgress();
+        progress.ifPresent(p -> System.out.println(p.getStudyDuration()));
 
     }
 }
